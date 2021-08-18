49968eb5bbca201a155c7fe60ec3957308a1c2c5
ShivanshiModi <87198158+ShivanshiModi@users.noreply.github.com>
2021-08-18 23:29:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function A(){
+    var name1 = document.getElementById("name_1").value;
+    console.log(name1);
+    var name2 = document.getElementById("name_2").value;
+    console.log(name2);
+    var full_name = name1.concat(name2);
+    console.log(full_name);
+    
+
+}
+function B(){
+    var i = document.getElementById("a").value;
+    console.log(i);
+    var j = document.getElementById("b").value;
+    console.log(j);
+    var k =  i+j;
+    console.log(k);
+}
